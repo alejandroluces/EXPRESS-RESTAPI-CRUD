166c0c805b31313a24f7dadd4793223854da684c
Alejandro Luces González <aluces@polytex.local>
2023-11-14 12:46:43 -0300

    Add_Information

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ let products =[{
     price: 120
 }]
 
-
 const app = express()
 
+//Settings
+app.set("case sensitive routing", true)
+app.set("appName", "Express Course")
+app.set("port", 4000)
+
+
+//Middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+
+//Routes
 //Enviare una peticion GET y respondere con un [  ]
+
 app.get("/producs", (req,res) => {    
     res.json(products)
+    // res.send("Hello World")
 })
 
 
@@ -29,8 +39,24 @@ app.post("/producs", (req,res) => {
 
 
 
-app.put("/producs", (req,res) => {
-    res.send("Actualizando productos")
+app.put("/producs/:id", (req,res) => {
+    const newData=req.body
+    const findProducID = products.find((item)=> item.id === Number(req.params.id))
+    console.log(findProducID);
+    
+    if(!findProducID ){
+        return res.status(404).json({
+            message: "Produc NO found"
+        })
+    }
+
+    products = products.map((item) => item.id === Number(req.params.id) ?{...item, ...newData} : item)
+    console.log(products);
+    
+
+    res.json({
+        message: "Produc Update successfully"
+    })
 })
 
 app.delete("/producs/:id", (req,res) => {
@@ -63,5 +89,5 @@ app.get("/producs/:id", (req,res) => {
 })
 
 
-app.listen(3000)
-console.log(`Server on port ${3000}`);
+app.listen(4000)
+console.log(`${app.get("appName")} on port ${4000}`);
